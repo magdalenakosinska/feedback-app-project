7ec5d9df2759747985061ac8bab7136757a727bc
magdalenakosinska <59508924+magdalenakosinska@users.noreply.github.com>
2023-03-02 16:16:38 +0100

    delete button working

--- src/components/FeedbackItem.js
+++ src/components/FeedbackItem.js
@@ -2,11 +2,11 @@ import { useState } from "react";
 import Card from "./shared/Card";
 import { FaTimes } from "react-icons/fa";
 
-function FeedbackItem({ item }) {
+function FeedbackItem({ item, handleDelete }) {
   return (
     <Card>
       <div className="num-display">{item.rating}</div>
-      <button className="close">
+      <button onClick={() => handleDelete(item.id)} className="close">
         <FaTimes color="purple" />
       </button>
       <div className="text-display">{item.text}</div>
